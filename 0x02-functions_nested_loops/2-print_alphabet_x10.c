fabf50b703d898624b6b05639314b034b27d60b3
ed004th <eizuagie@gmail.com>
2022-12-14 05:37:25 -0800

    c programming

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /*
- * print_alphabet_x10 - print 10 times the alphabet, in lowercase
- *description: using print_alphabet_x10 as the main function
+ * print_alphabet_x10 - entry point
+ *Description: print 10 times the alhabet in lowercase.
 */
-void print_alphabet_x10(void);
+
+void print_alphabet_x10(void)
 {
 	int count = 0;
 	char letter;
