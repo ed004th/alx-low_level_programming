18b3b0a8ae219a104e2382785525001214accd7a
ed004th <eizuagie@gmail.com>
2022-12-16 05:55:08 -0800

    task 0

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isdigit - check if numbers are from 0 to 9
+ * @c: char to check
+ *
+ * Return: 1 or 0
+ *
+*/
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
+
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiply two numbers
+ * @a: Let a be the first number
+ * @b: Let b be the second number
+ *
+ * Return: numbers
+ *
+*/
+int mul(int a, int b)
+{
+	return(a*b);
+}
