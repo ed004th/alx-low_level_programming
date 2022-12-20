f1e8239c191988f5f2c27760a0cda279284e6fdd
ed004th <eizuagie@gmail.com>
2022-12-20 06:05:16 -0800

    task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,13 +2,14 @@
 
 /**
  * _strlen - a function that return the length of a string
- * @s - char to be checked
+ * @s: int to be checked
  *
- * Return: int
+ * Return: 0
 */
 int _strlen(char *s)
 {
 	int b;
+
 	for (b = 0; s[b] != '\0'; b++)
 		return (b);
 }
