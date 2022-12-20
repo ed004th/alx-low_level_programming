4223bbb665bceead9db5cfa6c49b80d631537423
ed004th <eizuagie@gmail.com>
2022-12-20 12:49:21 -0800

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,20 +1,21 @@
 #include "main.h"
 /**
- * puts2 - a function that prints every other character of a string, starting with the first character
- * @str - char to checked
- * Return: void
+ * puts2 - a function that prints every other character of a string,
+ * @str: char to checked
+ * Return: string
  *
 */
 void puts2(char *str)
 {
 	int i;
+
 	while (str[i] != '\0')
 	{
 		if (i % 2 == 0)
 		{
 			_putchar (str[i]);
 		}
-			i++
+			i++;
 	}
 	_putchar ('\n');
 }
